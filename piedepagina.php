f9e3665db8ef8f61b2f1a274e2f28c0e24b8b3e2
epomasque <70190069+epomasque@users.noreply.github.com>
2024-06-01 13:03:06 -0500

    Add files via upload

---
+++ piedepagina.php
@@ -0,0 +1,81 @@
+<!-- footer.php -->
+<footer>
+      <!-- Pie de Pagina -->
+      <footer class="footer_style_2">
+         <div class="footer_top">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 margin_bottom_30">
+                     <div class="full width_9" style="margin-bottom:25px;"> <a href="index.html"><img class="img-responsive" width="250" src="images/logo.png" alt="#"></a> </div>
+                     <div class="full width_9">
+                        <p>Quienes representamos a RAUKHAY ENERGY damos soluciones integrales de capacitación y servicios varios, con un alto grado de profesionalismo, tecnología de vanguardia, responsabilidad social, protección del medio ambiente y altos estándares Nacionales e Internacionales de Calidad.</p>
+                     </div>
+                     <div class="full width_9">
+                        <p>Gracias a nuestro equipo de personas y empresas, contamos con una variedad de temáticas de capacitación, además de profesionales con alto grado de experiencia capacitando en diferentes áreas y prestación de servicios. </p>
+                     </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 margin_bottom_30">
+                     <div class="full">
+                        <div class="footer_blog_2 width_9">
+                           <h3>Twitter Feed</h3>
+                           <p><i class="fa fa-twitter"></i> @RaukhayEnergy - 15 mins 
+                              La Ley de Ohm explica la relación entre voltaje (V), corriente (I) y resistencia (R)_¡Aprendamos! #LeyDeOhm #Electrónica #Ciencia https://twitter.com/RaukhayEnergy
+                           </p>
+                           <p><i class="fa fa-twitter"></i> @RaukhayEnergy - 48 mins
+                              ¿Sabías que existen dos tipos  de corriente eléctrica? 📷📷_ ¡Aprendamos! @raukhayenergy https://twitter.com/RaukhayEnergy
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 margin_bottom_30">
+                     <div class="full">
+                        <div class="footer_blog_2">
+                           <h3>Social</h3>
+                        </div>
+                     </div>
+                     <div class="full">
+                        <ul class="footer-links">
+                           <li><a href="#"><i class="fa fa-facebook"></i> 5 Likes</a></li>
+                           <li><a href="#"><i class="fa fa-instagram"></i> 18 Post</a></li>
+                           <li><a href="#"><i class="fa fa-twitter"></i> 1,258 Followers</a></li>
+                           <li><a href="#"><i class="fa fa-linkedin"></i> 38+ Proyectos</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 margin_bottom_30">
+                     <div class="full">
+                        <div class="footer_blog_2 width_9">
+                           <h3>Blog</h3>
+                        </div>
+                        <div class="blog_post_footer">
+                           <div class="blog_post_img"> <img width="80" height="80" src="images/scr1.png" alt="#"> </div>
+                           <div class="blog_post_cont">
+                              <p class="date">marzo 22, 2024</p>
+                              <p class="post_head">La Importancia de los Diagramas P&ID en la Ingeniería y la Industria</p>
+                           </div>
+                        </div>
+                        <div class="blog_post_footer">
+                           <div class="blog_post_img"> <img width="80" height="80" src="images/scr2.png" alt="#"> </div>
+                           <div class="blog_post_cont">
+                              <p class="date">marzo 5, 2024</p>
+                              <p class="post_head">El Petróleo Alcanza Precios Históricos: Impacto en la Economía Global</p>
+                           </div>
+                        </div>
+                        <div class="blog_post_footer">
+                           <div class="blog_post_img"> <img width="80" height="80" src="images/scr3.png" alt="#"> </div>
+                           <div class="blog_post_cont">
+                              <p class="date">febrero 02, 2024</p>
+                              <p class="post_head">Fortaleciendo la Seguridad Industrial: Prioridad en el Entorno Laboral</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- footer bottom -->
+         <div class="footer_bottom">
+            <p>Dessigned and developed by <strong>Raukhay-Energy</strong></p>
+         </div>
+      </footer>
+</footer>
